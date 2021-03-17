70543b3cfc794555053e61fafd4321dc0cdd019f
Kotoba Trilius <38567918+KotobaSuke@users.noreply.github.com>
2021-03-17 18:03:50 +0800

    Update Substring_extraction.java

--- exempla/Substring_extraction.java
+++ exempla/Substring_extraction.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class Substring_extraction {
 	public static void main (String arg[]) {
 		try (Scanner input = new Scanner(System.in)) {
+			System.out.println("Enter a line:");
 			String mstr = input.nextLine();
-			int fromI = input.nextInt();
+			System.out.println("Enter a beginning index and a ending index:")
+			int fromI = input.nextInt() - 1;
 			int toI = input.nextInt();
 			System.out.println("The substring of \""+ mstr + "\" from index " + fromI + " to " + toI + " is \"" + mstr.substring(fromI, toI) + "\" ");
 		}
