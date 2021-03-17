7c7f5fd7ad27290e28650b47b9daca1b41b6b927
Kotoba Trilius <38567918+KotobaSuke@users.noreply.github.com>
2021-03-17 18:00:52 +0800

    Update String_concatenation.java

--- exempla/String_concatenation.java
+++ exempla/String_concatenation.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class String_concatenation {
 	public static void main (String arg[]) {
 		try (Scanner scan = new Scanner(System.in)) {
+			System.out.println("Enter two strings without spaces:");
 			String cont1 = scan.next();
 			String cont2 = scan.next();
 			System.out.println(cont1.concat(cont2));
